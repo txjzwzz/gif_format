240f1ea9d283f46769a100707ce0751cdbac8773
Wei Zheng <wei.zheng@ihandysoft.com>
2016-10-20 19:57:45 +0800

    half of image data

--- gif_decode.py
+++ gif_decode.py
@@ -57,12 +57,55 @@ class GifDecoder(object):
         self.logical_screen_descriptor['pixel_aspect_ratio'] = struct.unpack('B', f.read(1))[0]
 
     def read_global_color_table(self, f):
+        """
+        读取图像的Global Color Table,数据流只有一块
+        :param f: 文件读取时的迭代器
+        :return:
+        """
         # 判断该字段是否存在
         if self.logical_screen_descriptor['global_color_table_flag'] != 1:
             return
         length = 3 * pow(2, self.logical_screen_descriptor['size_of_global_color_table']+1)
         for i in range(length):
             self.global_color_table.append(struct.unpack('B', f.read(1))[0])
+
+    @staticmethod
+    def read_image_descriptor(f):
+        """
+        读取图像的Image Descriptor, 每张图像有且仅有一个Image Descriptor
+        :param f: 文件读取时的迭代器
+        :return: 包含属性的dict
+        """
+        res = dict()
+        res['image_separator'] = '{:02x}'.format(f.read(1))
+        res['image_left_position'] = struct.unpack('H', f.read(2))[0]
+        res['image_top_position'] = struct.unpack('H', f.read(2))[0]
+        res['image_width'] = struct.unpack('H', f.read(2))[0]
+        res['image_height'] = struct.unpack('H', f.read(2))[0]
+        packed_fields = f.read(1)
+        res['local_color_table_flag'] = read_bits_value_from_bytes(packed_fields, 1, 1)
+        res['interlace_flag'] = read_bits_value_from_bytes(packed_fields, 2, 1)
+        res['sort_flag'] = read_bits_value_from_bytes(packed_fields, 3, 1)
+        res['reversed'] = read_bits_value_from_bytes(packed_fields, 4, 2)
+        res['size_of_local_color_table'] = read_bits_value_from_bytes(packed_fields, 6, 3)
+        return res
+
+    @staticmethod
+    def read_local_color_table(f, image_descriptor_dict):
+        """
+        读取图像的Local Color Table
+        :param f: 文件读取时的迭代器
+        :param image_descriptor_dict: 在Local Color Table之前的Image Descriptor的Dict
+        :return: 包含颜色bytes的list
+        """
+        local_color_datas = []
+        if image_descriptor_dict['local_color_table_flag'] != 1:
+            return local_color_datas
+        size = image_descriptor_dict['size_of_local_color_table']
+        length = 3 * pow(2, size+1)
+        for i in range(length):
+            local_color_datas.append(struct.unpack('B', f.read(1))[0])
+        return local_color_datas
 
 
 if __name__ == '__main__':
