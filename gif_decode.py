639740bc5f650379ed0d4c8d0ebd20c42bdae907
Wei Zheng <wei.zheng@ihandysoft.com>
2016-10-20 09:48:27 +0800

    complete Logical Screen Descriptor and Global Color Table

--- gif_decode.py
+++ gif_decode.py
@@ -3,12 +3,16 @@
 @author wei.zheng
 @date 2016.10.19
 """
+import struct
+from gif_helpers import read_bits_value_from_bytes
 
 
 class GifDecoder(object):
 
     def __init__(self):
-        pass
+        self.header = dict()
+        self.logical_screen_descriptor = dict()
+        self.global_color_table = []
 
     def read_gif(self, filepath):
         """
@@ -17,8 +21,48 @@ class GifDecoder(object):
         :return:
         """
         with open(filepath, 'rb') as f:
-            self.header = {"Signature": f.read(3), "Version": f.read(3)}
+            self.read_head(f)
             print self.header
+            self.read_logical_screen_descriptor(f)
+            print self.logical_screen_descriptor
+            self.read_global_color_table(f)
+            print self.global_color_table
+
+    def read_head(self, f):
+        """
+        读取gif的header
+        :param f: 文件读取时的迭代器
+        :return:
+        """
+        self.header = {'Signature': f.read(3), 'Version': f.read(3)}
+
+    def read_logical_screen_descriptor(self, f):
+        """
+        读取gif的Logical Screen Descriptor
+        :param f: 文件读取时的迭代器
+        :return:
+        """
+        self.logical_screen_descriptor['logical_screen_width'] = struct.unpack('H', f.read(2))[0]
+        self.logical_screen_descriptor['logical_screen_height'] = struct.unpack('H', f.read(2))[0]
+        packed_fields = f.read(1)
+        self.logical_screen_descriptor['global_color_table_flag'] = read_bits_value_from_bytes(
+                                                                            packed_fields, 1, 1)
+        self.logical_screen_descriptor['color_resolution'] = read_bits_value_from_bytes(
+                                                                            packed_fields, 2, 3)
+        self.logical_screen_descriptor['sort_flag'] = read_bits_value_from_bytes(
+                                                                            packed_fields, 5, 1)
+        self.logical_screen_descriptor['size_of_global_color_table'] = read_bits_value_from_bytes(
+                                                                            packed_fields, 6, 3)
+        self.logical_screen_descriptor['background_color_index'] = struct.unpack('B', f.read(1))[0]
+        self.logical_screen_descriptor['pixel_aspect_ratio'] = struct.unpack('B', f.read(1))[0]
+
+    def read_global_color_table(self, f):
+        # 判断该字段是否存在
+        if self.logical_screen_descriptor['global_color_table_flag'] != 1:
+            return
+        length = 3 * pow(2, self.logical_screen_descriptor['size_of_global_color_table']+1)
+        for i in range(length):
+            self.global_color_table.append(struct.unpack('B', f.read(1))[0])
 
 
 if __name__ == '__main__':
---
+++ gif_helpers.py
@@ -0,0 +1,21 @@
+# -*- coding=utf-8 -*-
+"""
+辅助函数
+@author wei.zheng
+@date 2016.10.20
+"""
+
+
+def read_bits_value_from_bytes(byte_, start_index, length):
+    """
+    从byte中读取某几位bit的数值
+    :param byte_: 待读取的byte
+    :param start_index: 开始的索引下标
+    :param length: 读取的长度
+    :return: 数值
+    """
+    val_ = 0
+    byte = ord(byte_)
+    for i in range(start_index, start_index+length):
+        val_ = val_ * 2 + ((byte >> i) & 1)
+    return val_
